ae9b63dc234240e762c1533674cbce9b0c7e9800
Gilmar Justo <gilmar.justo2@gmail.com>
2019-11-12 16:20:58 -0300

    Update challenge-09.js

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -10,11 +10,11 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+    var number1 = 10;
+    var number2 = 20;
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
     return number1 + number2;
-    var number2 = 20;
 }
 myFunction();
 
@@ -22,12 +22,12 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
-    var sum = function sum() {
-        return number1 + number2;
-    };
     var number1 = 10;
     var number2 = 20;
+    var sum = function sum() {
+    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+    return number1 + number2;
+    };
     return sum();
 }
 myFunction2();
@@ -36,10 +36,10 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
-    var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
+    var number2 = 50;    
     var number1 = 40;
+    console.log( 'A soma de 40 e 50 é igual a ', sum() );
+    console.log( 'Na função myFunction3, number1 é igual a ', number1 );
     return sum();
     function sum() {
         return number1 + number2;
@@ -105,3 +105,4 @@ console.log( 'O resultado da divisão é:' );
 
 console.log( 'O resto da divisão é:' );
 // ?
+)();
